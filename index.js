7279954a2c702f5a118916423e0226eb64bc90b9
JP1005YT <godlolpro32@gmail.com>
2023-05-21 13:47:44 -0300

    Mega Commit

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
-let stars = 150
+// Estrelas
 
+let stars = 150
 for (let i = 0; i < stars; i++) {
     let obj = document.createElement('i')
+    let top = Math.random() * innerHeight- 5 
+    obj.style.top = top + "px"
+    let left = Math.random() * innerWidth- 5 
+    obj.style.left = left + "px"
+    if(top > innerHeight/2 && left > innerWidth/2){
+        obj.style.animation = 'sai_bottom_right 4s infinite'
+    }
+    if(top < innerHeight/2 && left > innerWidth/2){
+        obj.style.animation = 'sai_top_right 4s infinite'
+    }
+    if(top > innerHeight/2 && left < innerWidth/2){
+        obj.style.animation = "sai_bottom_left 4s infinite"
+    }
+    if(top < innerHeight/2 && left < innerWidth/2){
+        obj.style.animation = "sai_top_left 4s infinite"
+    }
     obj.style.animationDelay = Math.random() * 5 + "s"
-    obj.style.top = Math.random() * innerHeight- 5 + "px"
-    obj.style.left = Math.random() * innerWidth- 5 + "px"
     document.querySelector('body').appendChild(obj)
 }
 
+console.log(innerHeight/2)
+console.log(innerWidth/2)
+
+// Botoes
+
 let lis = document.querySelectorAll("li")
 lis.forEach(element => "")
+
+const botoes = document.querySelectorAll("li")
+botoes.forEach(element => {
+    element.addEventListener("click",e =>{
+        switch (element.innerHTML) {
+            case "Films":
+                window.location.href = "./pages/Films/";
+            break;
+            case "Peoples":
+                window.location.href = "./pages/Films/";
+            break;
+            case "Planets":
+                window.location.href = "./pages/Films/";
+            break;
+            case "Species":
+                window.location.href = "./pages/Films/";
+            break;
+            case "Starships":
+                window.location.href = "./pages/Films/";
+            break;
+            case "Vehicles":
+                window.location.href = "./pages/Films/";
+            break;
+        }
+    })
+})
---
+++ pages/Films/main.js
@@ -0,0 +1,106 @@
+url = "https://swapi.dev/api/films/"
+
+const queryString = window.location.search
+const params = new URLSearchParams(queryString)
+
+if(params.has("id")){
+    pegarPessoasPorId(params.get("id"))
+}else{
+    pegarPessoas()
+}
+
+async function pegarPessoas(){
+    const dados = await fetch(url)
+    .then((resultado) => {
+        return resultado.json();
+    })
+    ConstruirTodos(dados)
+}
+
+async function pegarPessoasPorId(id){
+    const dados = await fetch(url + id)
+    .then((resultado) => {
+        return resultado.json();
+    })
+    ConstruirCada(dados)
+}
+
+async function pegarInfos(link) {
+    const resposta = await fetch(link);
+    const dados = await resposta.json();
+    return dados;
+  }  
+
+function ConstruirTodos(array){
+    array.results.forEach(function(element,n){
+        let number = n + 1
+        let span = document.createElement('span')
+        span.innerHTML = element.title
+        span.addEventListener("click",e =>{
+            window.location.href = `http://localhost/starWars2.0/pages/Films?id=${number}`
+        })
+        document.querySelector("body").appendChild(span)
+    });
+}
+
+async function ConstruirCada(array){
+    // Item que é a pagina
+    console.log(array)
+    document.querySelector("#one").classList.toggle("ativo")
+    let objetos = Object.keys(array)
+    objetos.forEach(element =>{
+        // Cada objeto ou texto desse item
+        if(document.querySelector(`#${element}`)){
+            if(typeof array[element] === "object"){
+                // Se for Objeto
+                let div = document.createElement("div")
+                array[element].forEach(async function(link){
+                    // Faz o Fetch e puxa o nome do bixo
+                    let resposta = await pegarInfos(link)
+                    let url = resposta['url'].slice(0, -1);
+                    let ultimoDigito = url.slice(-1).charAt(0);
+                    switch (element) {
+                        // esse switch monta link por link
+                        case "planets":
+                            let link = document.createElement("a")
+                            link.setAttribute("href",`http://localhost/starWars2.0/pages/${element}?id=${ultimoDigito}`)
+                            link.innerHTML = resposta['name']
+                            document.querySelector(`#${element}`).appendChild(link)
+                            break;
+                        case "species":
+                            let links = document.createElement("a")
+                            links.setAttribute("href",`http://localhost/starWars2.0/pages/${element}?id=${ultimoDigito}`)
+                            links.innerHTML = resposta['name']
+                            document.querySelector(`#${element}`).appendChild(links)
+                            break
+                        case "starships":
+                            let linkst = document.createElement("a")
+                            linkst.setAttribute("href",`http://localhost/starWars2.0/pages/${element}?id=${ultimoDigito}`)
+                            linkst.innerHTML = resposta['name']
+                            document.querySelector(`#${element}`).appendChild(linkst)
+                            break
+                        case "vehicles":
+                                let linkv = document.createElement("a")
+                                linkv.setAttribute("href",`http://localhost/starWars2.0/pages/${element}?id=${ultimoDigito}`)
+                                linkv.innerHTML = resposta['name']
+                                document.querySelector(`#${element}`).appendChild(linkv)
+                                break
+                        case "characters":
+                                let linkc = document.createElement("a")
+                                linkc.setAttribute("href",`http://localhost/starWars2.0/pages/${element}?id=${ultimoDigito}`)
+                                linkc.innerHTML = resposta['name']
+                                document.querySelector(`#${element}`).appendChild(linkc)
+                                break
+                        default:
+                            break;
+                    }
+                })
+            }else{
+                // Se for texto cai nesse
+                let span = document.createElement("span")
+                span.innerHTML = array[element]
+                document.querySelector(`#${element}`).appendChild(span)
+            }
+        }
+    })
+}
